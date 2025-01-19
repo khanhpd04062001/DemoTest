29c6b376b620c24d720797fa943d1e93837c1382
khanhcp891 <77950148+khanhcp891@users.noreply.github.com>
2025-01-19 19:35:34 +0700

    SQL

---
+++ SQLQuery2.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE SP
+GO
+USE SP
+GO
+CREATE TABLE SanPham (
+    SanPhamID INT PRIMARY KEY,
+    TenSanPham NVARCHAR(255) ,
+    Gia DECIMAL(18, 2) ,
+    NgayNhap DATE 
+);
+
+CREATE TABLE LoaiSanPham (
+    LoaiSanPhamID INT PRIMARY KEY,
+    TenLoai NVARCHAR(255) ,
+    NgayNhap DATE 
+);
+
+CREATE TABLE SanPham_LoaiSanPham (
+    SanPhamID INT ,
+    LoaiSanPhamID INT ,
+    PRIMARY KEY (SanPhamID, LoaiSanPhamID),
+    FOREIGN KEY (SanPhamID) REFERENCES SanPham(SanPhamID),
+    FOREIGN KEY (LoaiSanPhamID) REFERENCES LoaiSanPham(LoaiSanPhamID)
+);
